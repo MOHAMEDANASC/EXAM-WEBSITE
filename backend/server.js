48f6f0b3d5c581090efff7a36b644c558fd81315
MOHAMEDANASC <mohamedanasc@gmail.com>
2025-11-10 21:05:48 +0530

    deploying after changing the cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://online-exam-frontend-6yihodedm-mohamed-anas-cs-projects.vercel.app"
+  "https://online-exam-frontend-6yihodedm-mohamed-anas-cs-projects.vercel.app",
+  "*"
 ];
 
 // ✅ FIXED — Always handle OPTIONS preflight
